1cf66a8b1f2b7c924d0d950a576021c7301ac416
Rafael Sales <rafael.sales93@hotmail.com>
2020-09-13 22:34:11 -0300

    Using POST association 'belongs_to'

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,20 +1,19 @@
 class Contact < ApplicationRecord
-  belongs_to :kind, foreign_key: "kind_id"
+  belongs_to :kind, foreign_key: "kind_id", optional: true
 
-  def author
-    "Rafael Sales"
-  end
+  # def author
+  #   "Rafael Sales"
+  # end
 
-  def kind_description
-    self.kind.description
-  end
+  # def kind_description
+  #   self.kind.description
+  # end
   
-  
-  def as_json(options={})
-    super(
-      root: true,
-      methods: [:kind_description, :author], 
-      include: { kind: { only: :description }}
-    )
-  end
+  # def as_json(options={})
+  #   super(
+  #     root: true,
+  #     methods: [:kind_description, :author], 
+  #     include: { kind: { only: :description }}
+  #   )
+  # end
 end
