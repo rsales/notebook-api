1754083e7ccdb482bc8aeb8d10d3aecf69917d71
Rafael Sales <rafael.sales93@hotmail.com>
2020-09-14 10:23:09 -0300

    I18n using method 'as_json'.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,17 +3,23 @@ class Contact < ApplicationRecord
   belongs_to :kind, foreign_key: "kind_id" #, optional: true
   has_many :phones
 
+  def as_json(options={})
+    hash = super(options)
+    hash[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    hash
+  end
+
   # def birthdate_br
   #   I18n.l(self.birthdate) unless self.birthdate.blank?
   # end
 
-  def to_br
-    { 
-      name: self.name,
-      email: self.email,
-      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
-    }
-  end
+  # def to_br
+  #   { 
+  #     name: self.name,
+  #     email: self.email,
+  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+  #   }
+  # end
 
   # def author
   #   "Rafael Sales"
