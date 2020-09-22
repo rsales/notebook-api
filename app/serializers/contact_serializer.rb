29b171c44e2e57b67bba6af76c7de0d0d4736596
Rafael Sales <rafael.sales93@hotmail.com>
2020-09-21 22:40:14 -0300

    Include associations.

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,6 +1,10 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
+  belongs_to :kind
+  has_many :phones
+  has_one :address
+
   def attributes(*args)
     hash = super(*args)
     # hash[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
